aa31ea727ef99d33a7f923a1a650cdd5fc4bacfc
Katie Williams <kawilliams8@gmail.com>
2019-03-16 12:53:34 -0600

    Edit code to meet Turing style guides

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var headerSpan = document.querySelector("#header-span")
-var inputName = document.querySelector("#input-text")
-var nameButton = document.querySelector("#name-dog")
+var headerSpan = document.querySelector(".js-header-span")
+var inputName = document.querySelector(".js-input-text")
+var nameButton = document.querySelector(".js-name-dog")
 
 nameButton.addEventListener("click", function(e) {
  e.preventDefault()
